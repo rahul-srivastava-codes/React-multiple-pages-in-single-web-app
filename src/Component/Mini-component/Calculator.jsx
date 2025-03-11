31667dde86891c80f008703358d947db2b88227b
black-cat-developer <rahulsrichunar@gmail.com>
2025-03-11 19:40:31 +0530

    calculator created

--- src/Component/Mini-component/Calculator.jsx
+++ src/Component/Mini-component/Calculator.jsx
@@ -1,18 +1,135 @@
 import React, { useState } from "react";
 
 function Calculator() {
+  const [input, setinput] = useState();
+
+  function handleclick(value) {
+    if (value === "=") {
+      try {
+        setinput(eval(input)); // Evaluates the expression
+      } catch (error) {
+        setinput("Error"); // For invalid expressions
+      }
+    } else if (value === "AC") {
+      setinput(""); // Clears the input
+    } else {
+      setinput((prev) => (prev ? prev + value : value)); // Concatenate input
+    }
+  }
   return (
     <div>
       <form
+        onSubmit={(e) => e.preventDefault()}
         action=""
         className="flex  flex-col items-center justify-center  h-[90vh] w-[100vw]"
       >
-        <input
-          type="text"
-          className="w-[30vw] h-[10vh] border-1 rounded-lg"
-          value={input}
-          onChange={() => setinput(e.target.value)}
-        />
+        <div>
+          <input
+            type="text"
+            className="w-[30vw] h-[10vh] border-1 rounded-lg"
+            value={input}
+          />
+        </div>
+        <div className="mt-2 flex items-center justify-between w-[30vw] h-[10vh]">
+          <button
+            className="mx-1 rounded-lg bg-zinc-400 w-[10vw] h-[10vh]"
+            onClick={() => handleclick("1")}
+          >
+            1
+          </button>
+          <button
+            className="mx-1 rounded-lg w-[10vw] h-[10vh] bg-zinc-400"
+            onClick={() => handleclick("2")}
+          >
+            2
+          </button>
+          <button
+            className="mx-1 w-[10vw] h-[10vh] rounded-lg bg-zinc-400"
+            onClick={() => handleclick("3")}
+          >
+            3
+          </button>
+        </div>
+        <div className="mt-2 flex items-center justify-between w-[30vw] h-[10vh]">
+          <button
+            className="mx-1 rounded-lg bg-zinc-400 w-[10vw] h-[10vh]"
+            onClick={() => handleclick("4")}
+          >
+            4
+          </button>
+          <button
+            className="mx-1 rounded-lg w-[10vw] h-[10vh] bg-zinc-400"
+            onClick={() => handleclick("5")}
+          >
+            5
+          </button>
+          <button
+            className="mx-1 w-[10vw] h-[10vh] rounded-lg bg-zinc-400"
+            onClick={() => handleclick("6")}
+          >
+            6
+          </button>
+        </div>
+        <div className="mt-2 flex items-center justify-between w-[30vw] h-[10vh]">
+          <button
+            className="mx-1 rounded-lg bg-zinc-400 w-[10vw] h-[10vh]"
+            onClick={() => handleclick("7")}
+          >
+            7
+          </button>
+          <button
+            className="mx-1 rounded-lg w-[10vw] h-[10vh] bg-zinc-400"
+            onClick={() => handleclick("8")}
+          >
+            8
+          </button>
+          <button
+            className="mx-1 w-[10vw] h-[10vh] rounded-lg bg-zinc-400"
+            onClick={() => handleclick("9")}
+          >
+            9
+          </button>
+        </div>
+        <div className="mt-2 flex items-center justify-between w-[30vw] h-[10vh]">
+          <button
+            className="mx-1 rounded-lg bg-zinc-400 w-[10vw] h-[10vh]"
+            onClick={() => handleclick("+")}
+          >
+            +
+          </button>
+          <button
+            className="mx-1 rounded-lg w-[10vw] h-[10vh] bg-zinc-400"
+            onClick={() => handleclick("-")}
+          >
+            -
+          </button>
+          <button
+            className="mx-1 w-[10vw] h-[10vh] rounded-lg bg-zinc-400"
+            onClick={() => handleclick("*")}
+          >
+            *
+          </button>
+        </div>
+        <div className="mt-2 flex items-center justify-between w-[30vw] h-[10vh]">
+          <button
+            className="mx-1 rounded-lg bg-zinc-400 w-[10vw] h-[10vh]"
+            onClick={() => handleclick("/")}
+          >
+            /
+          </button>
+          <button
+            className="mx-1 rounded-lg w-[10vw] h-[10vh] bg-zinc-400"
+            onClick={() => handleclick("=")}
+          >
+            =
+          </button>
+          <button
+            className="mx-1 w-[10vw] h-[10vh] rounded-lg bg-zinc-400"
+            onClick={() => handleclick("AC")}
+          >
+            AC
+          </button>
+        </div>
       </form>
     </div>
   );
--- src/Component/Mini-component/Contact.jsx
+++ src/Component/Mini-component/Contact.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className=" ">
+      <div className="w-[100%] h-[90vh] flex items-center justify-center bg-green-300">
+        <div className="bg-blue-400 rounded-lg px-4 py-2">
+          <div>
+            LinkedIn :{"Click Me "}
+            <link
+              rel="stylesheet"
+              href="www.linkedin.com/in/rahul-srivastava-4605311a5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
